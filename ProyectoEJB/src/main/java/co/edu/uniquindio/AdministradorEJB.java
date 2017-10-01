7172f9e13f70285f4f127c534dd8a6843d0b8ec9
Capriatto <michu-1@hotmail.com>
2017-10-01 11:46:27 -0500

    avance guia 11 -C

---
+++ ProyectoEJB/src/main/java/co/edu/uniquindio/AdministradorEJB.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class AdministradorEJB
+ */
+@Stateless
+@LocalBean
+public class AdministradorEJB implements AdministradorEJBRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public AdministradorEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ProyectoEJB/src/main/java/co/edu/uniquindio/AdministradorEJBRemote.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface AdministradorEJBRemote {
+
+}
