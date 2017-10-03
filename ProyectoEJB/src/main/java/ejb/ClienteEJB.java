129d4c275910c575d6acfaa177b6b2016a87588e
Capriatto <michu-1@hotmail.com>
2017-10-03 17:48:29 -0500

    registrarConsultaTecnica -Capriatto

--- ProyectoEJB/src/main/java/ejb/ClienteEJB.java
+++ ProyectoEJB/src/main/java/ejb/ClienteEJB.java
@@ -1,15 +1,16 @@
 package ejb;
 
+
+import java.sql.Date;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
+import co.edu.uniquindio.com.Cliente;
+import co.edu.uniquindio.com.Consulta_tecnica;
 import co.edu.uniquindio.com.Empleado;
-import excepciones.ElementoRegistradorException;
-import excepciones.InformacionRepetidaException;
 
 /**
  * Session Bean implementation class administradorEJB
@@ -29,55 +30,27 @@ public class ClienteEJB {
 	private EntityManager entityManager;
 
 	/**
-	 * Metodo que permite registrar un empleado en la base de datos
-	 * 
-	 * @param cedula
-	 * @param apellido
-	 * @param contrasena
-	 * @param correo
+	 * Metodo que permite registrar una consulta técnica en la base de datos
+	 * @param id
+	 * @param consulta
 	 * @param estado
-	 * @param nombre
-	 * @param usuario
-	 * @return devuelve true si el empleado fue registrado con exito
-	 * @throws ElementoRegistradorException
-	 * @throws InformacionRepetidaException
+	 * @param f_consulta_tecnica
+	 * @param cliente
+	 * @param empleado
+	 * @return
 	 */
-	public boolean registroEmpleado(String cedula, String apellido, String contrasena, String correo, int estado,
-			String nombre, String usuario) throws ElementoRegistradorException, InformacionRepetidaException {
-		if (entityManager.find(Empleado.class, cedula) != null) {
-			throw new ElementoRegistradorException("Empleado ya fue registrado");
-		}
-		if (buscarEmpleadoPorNombreUsuario(usuario) != null) {
-			throw new InformacionRepetidaException("Usuario ya existe");
-		} else {
-			Empleado empleado = new Empleado();
-			empleado.setCedula(cedula);
-			empleado.setApellido(apellido);
-			empleado.setContrasena(contrasena);
-			empleado.setCorreo(correo);
-			empleado.setEstado(estado);
-			empleado.setNombre(nombre);
-			empleado.setUsuario(usuario);
-			entityManager.persist(empleado);
+	public boolean registroConsultaTecnica(String id, String consulta, int estado, Date f_consulta_tecnica, Cliente cliente,
+		    Empleado empleado) {
+			Consulta_tecnica consulta_tec = new Consulta_tecnica();
+			consulta_tec.setId(id);
+			consulta_tec.setConsulta(consulta);
+			consulta_tec.setEstado(estado);
+			consulta_tec.setF_consultatecnica(f_consulta_tecnica);
+			consulta_tec.setCliente_id(cliente);
+			consulta_tec.setEmpleado_id(empleado);;
+			entityManager.persist(consulta_tec);
 			return true;
-		}
-	}
-
-	/**
-	 * Permite buscar un empleado por su nombre de usuario
-	 * 
-	 * @param usuario
-	 *            Nombre del usuario
-	 * @return el usuario encontrado
-	 */
-	private Empleado buscarEmpleadoPorNombreUsuario(String usuario) {
-		try {
-			TypedQuery<Empleado> query = entityManager.createNamedQuery(Empleado.CREDENCIALES, Empleado.class);
-			query.setParameter("usuario", usuario);
-			return query.getSingleResult();
-		} catch (NoResultException e) {
-			return null;
-		}
+		
 	}
 
 }
--- ProyectoEJB/src/main/java/ejb/ClienteEJBRemote.java
+++ ProyectoEJB/src/main/java/ejb/ClienteEJBRemote.java
@@ -1,28 +1,35 @@
 package ejb;
 
+import java.sql.Date;
+
 import javax.ejb.Remote;
 
-import excepciones.ElementoRegistradorException;
-import excepciones.InformacionRepetidaException;
+import co.edu.uniquindio.com.Cliente;
+import co.edu.uniquindio.com.Empleado;
 
 /**
- * Interfaz que determina los metodos de acceso del EJB de administrador.
+ * Interfaz que determina los metodos de acceso del EJB de Cliente.
  * 
  * @author German
  *
  */
 @Remote
 public interface ClienteEJBRemote {
+	
 	/**
-	 * Permite registrar un empleado en la base de datos
-	 * 
-	 * @param Empleado
-	 *            Empleado a ser agregado.
-	 * @throws ElementoRegistradorException
-	 *             Al momento de intentar agregar un elemento ya existe
-	 * @throws InformacionRepetidaException
-	 *             Al momento de agregar informacion que ya pertenece a un empleado
+	 * Permite registrar un cliente en la base de datos
+	 * @param id
+	 * @param consulta
+	 * @param estado
+	 * @param f_consulta_tecnica
+	 * @param cliente
+	 * @param empleado
+	 * @return
 	 */
-	public boolean registroEmpleado(String cedula, String nombre, String email, String clave, int edad,
-			String nombreUsuario) throws ElementoRegistradorException, InformacionRepetidaException;
+	public boolean registroConsultaTecnica(String id, String consulta, int estado, Date f_consulta_tecnica, Cliente cliente,
+		    Empleado empleado);
+	
+	
 }
+
+
