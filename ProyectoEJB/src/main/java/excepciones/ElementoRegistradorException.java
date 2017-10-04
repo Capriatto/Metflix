1f3a0804acd1ca5ee6c221fde927761dbb3b17ab
cherman11 <valencia9405@gmail.com>
2017-10-03 19:21:11 -0500

    Guia 11 finish

--- ProyectoEJB/src/main/java/excepciones/ElementoRegistradorException.java
+++ ProyectoEJB/src/main/java/excepciones/ElementoRegistradorException.java
@@ -1,21 +1,14 @@
 package excepciones;
 
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
+public class ElementoRegistradorException extends Exception {
+	private static final long serialVersionUID = 1;
 
-/**
- * Session Bean implementation class ElementoRegistradorException
- */
-@Stateless
-@LocalBean
-public class ElementoRegistradorException extends Exception{
-private static final long serialVersionUID=1;
-    /**
-     * Default constructor. 
-     */
-    public ElementoRegistradorException(String mensaje) {
-    	super(mensaje);
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public ElementoRegistradorException(String mensaje) {
+		super(mensaje);
+		// TODO Auto-generated constructor stub
+	}
 
 }
