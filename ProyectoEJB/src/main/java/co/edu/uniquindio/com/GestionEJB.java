235b04528d4622d6d13ed8301dbdf444c405d2e2
Capriatto <michu-1@hotmail.com>
2017-10-17 18:08:20 -0500

    quitando linea de archivo persistence.xml

--- ProyectoEJB/src/main/java/co/edu/uniquindio/com/GestionEJB.java
+++ ProyectoEJB/src/main/java/co/edu/uniquindio/com/GestionEJB.java
@@ -1,24 +1,24 @@
-package co.edu.uniquindio.com;
-
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-/**
- * Session Bean implementation class GestionEJB
- */
-@Stateless
-@LocalBean
-public class GestionEJB implements GestionEJBRemote {
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	/**
-	 * Default constructor.
-	 */
-	public GestionEJB() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
+//package co.edu.uniquindio.com;
+//
+//import javax.ejb.LocalBean;
+//import javax.ejb.Stateless;
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+///**
+// * Session Bean implementation class GestionEJB
+// */
+//@Stateless
+//@LocalBean
+//public class GestionEJB implements GestionEJBRemote {
+//	@PersistenceContext
+//	private EntityManager entityManager;
+//
+//	/**
+//	 * Default constructor.
+//	 */
+//	public GestionEJB() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//}
--- ProyectoEJB/src/main/java/co/edu/uniquindio/com/GestionEJBRemote.java
+++ ProyectoEJB/src/main/java/co/edu/uniquindio/com/GestionEJBRemote.java
@@ -1,8 +1,8 @@
-package co.edu.uniquindio.com;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface GestionEJBRemote {
-String JNDI = "java:global/ProyectoEAR-Metflix/ProyectoEJB/GestionEJB!co.edu.uniquindio.com.GestionEJBRemote";
-}
+//package co.edu.uniquindio.com;
+//
+//import javax.ejb.Remote;
+//
+//@Remote
+//public interface GestionEJBRemote {
+//String JNDI = "java:global/ProyectoEAR-Metflix/ProyectoEJB/GestionEJB!co.edu.uniquindio.com.GestionEJBRemote";
+//}
