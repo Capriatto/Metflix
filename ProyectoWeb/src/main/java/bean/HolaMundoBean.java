d604630dd004ea9b5323205c735b2ca94901cf26
cherman11 <valencia9405@gmail.com>
2017-10-17 18:58:36 -0500

    Cambios, Al dia guia 14

--- ProyectoWeb/src/main/java/bean/HolaMundoBean.java
+++ ProyectoWeb/src/main/java/bean/HolaMundoBean.java
@@ -1,10 +1,15 @@
 package bean;
 
 import javax.annotation.ManagedBean;
-
+/**
+ * permite mejorar la informacion del archivo xhtml de saludo
+ * @author German
+ * @version 1.0
+ *
+ */
 @ManagedBean
 public class HolaMundoBean {
-	String mensaje = "Hola Mundo";
+	 private String mensaje = "Hola Mundo 2";
 
 	public String getMensaje() {
 		return mensaje;
